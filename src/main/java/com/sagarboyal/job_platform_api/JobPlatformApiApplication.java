b5616ff9552ee064e90bb1a4fa563ed56dbf0edd
sagarboyal <sboyal12@gmail.com>
2026-02-09 19:40:59 +0530

    project created via spring boot

---
+++ src/main/java/com/sagarboyal/job_platform_api/JobPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.sagarboyal.job_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagarboyal/job_platform_api/JobPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagarboyal.job_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
