9907f8843ed6d44da48cb02205c70ae3fe63f3a7
sagarboyal <sboyal12@gmail.com>
2026-02-14 21:52:13 +0530

    add provider table

---
+++ src/main/java/com/sagarboyal/job_platform_api/scrapper/modal/Provider.java
@@ -0,0 +1,44 @@
+package com.sagarboyal.job_platform_api.scrapper.modal;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "providers")
+public class Provider {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String name;
+
+    @Column(nullable=false, unique=true)
+    private String fullName;
+
+    @Column(nullable=false, length = 1000)
+    private String url;
+
+    private boolean active;
+
+    private int frequencyMinutes;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        active = true;
+        frequencyMinutes = 60;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
